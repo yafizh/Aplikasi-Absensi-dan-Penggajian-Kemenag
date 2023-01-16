b7ceaa25a1eb8af5486efbe5fe401f74ad59ce1c
A Chairil Yafizh <a.chairil.yafizh@gmail.com>
2023-01-16 09:07:58 +0700

    partials

---
+++ templates/footer.php
@@ -0,0 +1,24 @@
+</div>
+
+<aside class="control-sidebar control-sidebar-dark">
+</aside>
+</div>
+
+<script src="assets/js/adminlte.min.js"></script>
+<script src="assets/js/demo.js"></script>
+<script>
+  $(function() {
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": true,
+      "searching": true,
+      "ordering": false,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
+    });
+  });
+</script>
+</body>
+
+</html>
