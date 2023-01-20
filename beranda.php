930ca8f970ea39187d8066d672b68448fa7b99f5
A Chairil Yafizh <a.chairil.yafizh@gmail.com>
2023-01-20 09:24:12 +0700

    udpate dashboard admin

--- beranda.php
+++ beranda.php
@@ -1,14 +1,8 @@
 <section class="content-header">
     <div class="container-fluid">
         <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1>Dashboard</h1>
-            </div>
-            <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item active">Dashboard</li>
-                </ol>
+            <div class="col-12">
+                <h1>Selamat Datang Admin</h1>
             </div>
         </div>
     </div>
