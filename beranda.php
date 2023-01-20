7dea6a8bb5e53b782e983fa2c066cf0f24fe0df1
A Chairil Yafizh <a.chairil.yafizh@gmail.com>
2023-01-20 09:29:03 +0700

    update dashboard

--- beranda.php
+++ beranda.php
@@ -14,57 +14,56 @@
         <div class="row">
             <div class="col-lg-3 col-6">
 
+                <?php $pegawai = $mysqli->query("SELECT * FROM pegawai"); ?>
                 <div class="small-box bg-info">
                     <div class="inner">
-                        <h3>150</h3>
+                        <h3><?= $pegawai->num_rows; ?></h3>
                         <p>Pegawai</p>
                     </div>
                     <div class="icon">
                         <i class="ion ion-bag"></i>
                     </div>
-                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
 
             <div class="col-lg-3 col-6">
 
+                <?php $admin = $mysqli->query("SELECT * FROM user WHERE status='ADMIN'"); ?>
                 <div class="small-box bg-success">
                     <div class="inner">
-                        <h3>53<sup style="font-size: 20px">%</sup></h3>
-                        <p>Bounce Rate</p>
+                        <h3><?= $admin->num_rows; ?></h3>
+                        <p>Admin</p>
                     </div>
                     <div class="icon">
                         <i class="ion ion-stats-bars"></i>
                     </div>
-                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
 
             <div class="col-lg-3 col-6">
 
+                <?php $jabatan = $mysqli->query("SELECT * FROM jabatan"); ?>
                 <div class="small-box bg-warning">
                     <div class="inner">
-                        <h3>44</h3>
-                        <p>User Registrations</p>
+                        <h3><?= $jabatan->num_rows; ?></h3>
+                        <p>Jabatan</p>
                     </div>
                     <div class="icon">
                         <i class="ion ion-person-add"></i>
                     </div>
-                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
 
             <div class="col-lg-3 col-6">
-
+                <?php $tunjangan = $mysqli->query("SELECT * FROM tunjangan"); ?>
                 <div class="small-box bg-danger">
                     <div class="inner">
-                        <h3>65</h3>
-                        <p>Unique Visitors</p>
+                        <h3><?= $tunjangan->num_rows; ?></h3>
+                        <p>Tunjangan</p>
                     </div>
                     <div class="icon">
                         <i class="ion ion-pie-graph"></i>
                     </div>
-                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
         </div>
